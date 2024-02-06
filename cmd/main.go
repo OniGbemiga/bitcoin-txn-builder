9a2e7dba818e706502cc02b611f2cb8f8876766f
Gbemiga <onigbenga76@gmail.com>
2024-02-06 11:54:58 +0100

    fixed transaction signing bug

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/hex"
 	"flag"
 	"fmt"
-	"github.com/btcsuite/btcd/btcec/v2"
 	"github/OniGbemiga/bitcoin-txn-builder/internals"
 	"log"
 )
@@ -13,10 +11,10 @@ func main() {
 	// Parse command line arguments
 	taskPtr := flag.String("task", "", "Specify the task: redeem, derive, transaction, spending")
 	preImagePtr := flag.String("preimage", "", "Specify the pre-image for redeeming")
-	destAddressPtr := flag.String("dest", "", "Specify the destination address for spending")
-	changeAddressPtr := flag.String("change", "", "Specify the change address for spending")
+	destAddressPtr := flag.String("dest", "", "Specify the destination address for transaction")
 	amountPtr := flag.Int64("amount", 0, "Specify the amount for transaction")
-	privateKey := flag.String("private-key", "", "Specify the private key for transaction")
+	privateKeyPtr := flag.String("private-key", "", "Specify the private key for transaction")
+	txHexPtr := flag.String("txhex", "", "Specify the transaction hex for transaction")
 	flag.Parse()
 
 	switch *taskPtr {
@@ -33,23 +31,24 @@ func main() {
 		fmt.Println("Derived Address:", address)
 
 	case "transaction":
-		privateKey := parsePrivateKey(*privateKey)
-		address, _ := internals.GetAddressFromRedeemScriptHex(internals.GenerateRedeemScriptHex(*preImagePtr))
-		transactionHex, err := internals.ConstructTransaction(address, privateKey, *amountPtr)
+		destAddress := *destAddressPtr
+		privateKey := *privateKeyPtr
+		redeemScriptHex := internals.GenerateRedeemScriptHex(*preImagePtr)
+		transactionHex, err := internals.ConstructTransaction(redeemScriptHex, destAddress, privateKey, *amountPtr)
 		if err != nil {
 			log.Fatal("Error constructing transaction:", err)
 		}
 		fmt.Println("Constructed Transaction Hex:", transactionHex)
 
 	case "spending":
-		privateKey := parsePrivateKey(*privateKey)
-		originalTransactionHex := "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0" +
-			"401e80300000000001600140f0754d8e59a359ba426e103ea4cf4a084c190c4f53e1f02000000001600140f0754d8e59a359ba426" +
-			"e103ea4cf4a084c190c4ffffffff012c6c0000000000001976a914a72ecf0ca3a2a9d8545a0e49ef7898e64c4c76cf88ac00000000"
+		privateKey := *privateKeyPtr
+		originalTransactionHex := *txHexPtr
 		redeemScriptHex := internals.GenerateRedeemScriptHex(*preImagePtr)
-		destAddress := *destAddressPtr
-		changeAddress := *changeAddressPtr
-		spendingTransactionHex, err := internals.SpendTransactionOriginalTxHex(originalTransactionHex, redeemScriptHex, destAddress, changeAddress, privateKey)
+		changeAddress, err := internals.GetAddressFromRedeemScriptHex(redeemScriptHex)
+		if err != nil {
+			log.Fatal("Error deriving change address:", err)
+		}
+		spendingTransactionHex, err := internals.SpendTransactionOriginalTxHex(originalTransactionHex, redeemScriptHex, changeAddress, privateKey)
 		if err != nil {
 			log.Fatal("Error constructing spending transaction:", err)
 		}
@@ -60,20 +59,6 @@ func main() {
 	}
 }
 
-func parsePrivateKey(privateKeyHx string) *btcec.PrivateKey {
-	// Decode the private key from hex
-	privateKeyBytes, err := hex.DecodeString(privateKeyHx)
-	if err != nil {
-		return nil
-	}
-
-	// Parse the private key
-	privateKey, _ := btcec.PrivKeyFromBytes(privateKeyBytes)
-
-	return privateKey
-
-}
-
 //# Example for redeem task
 //go run main.go -task=redeem -preimage="Btrust Builders"
 //
@@ -81,7 +66,12 @@ func parsePrivateKey(privateKeyHx string) *btcec.PrivateKey {
 //go run main.go -task=derive -preimage="Btrust Builders"
 //
 //# Example for transaction task
-//go run main.go -task=transaction -preimage="Btrust Builders" -amount=100000
+//go run main.go -task=transaction -preimage="Btrust Builders" -dest="" private-key="" -amount=100000
 //
 //# Example for spending task
-//go run main.go -task=spending -preimage="Btrust Builders" -dest="your_destination_address" -change="your_change_address"
+//go run main.go -task=spending -preimage="Btrust Builders" private-key="", txHexPtr=""
+
+//2N4dyn5ZzEuw61YJjSVxHM19EbGxjT8v5Ze
+//2NEGYjZXxXxM8Vg4X1MpfRCxSdjrzM8sZgT
+
+//private-key
--- internals/repository.go
+++ internals/repository.go
@@ -3,9 +3,7 @@ package internals
 import (
 	"bytes"
 	"crypto/ecdsa"
-	"crypto/sha256"
 	"encoding/hex"
-	"fmt"
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
@@ -18,142 +16,137 @@ import (
 type TxnBuilderRepository interface {
 	GenerateRedeemScriptHex(preImage string) string
 	GetAddressFromRedeemScriptHex(redeemScriptHex string) (string, error)
-	ConstructTransaction(address string, privateKey *ecdsa.PrivateKey, amount int64) (string, error)
+	ConstructTransaction(redeemScriptHex string, address string, privateKey *ecdsa.PrivateKey, amount int64) (string, error)
 	SpendTransactionOriginalTxHex(txHex string, redeemScriptHex string, destAddress string,
 		changeAddress string, privateKey *ecdsa.PrivateKey) (string, error)
 }
 
-func GenerateRedeemScriptHex(preImage string) string {
-	lockingScript := sha256.Sum256([]byte(preImage))
-	return fmt.Sprintf("OP_SHA256%xOP_EQUAL", lockingScript)
+func GenerateRedeemScriptHex(preImage string) []byte {
+	script, _ := txscript.NewScriptBuilder().
+		AddOp(txscript.OP_SHA256).
+		AddData([]byte(preImage)).
+		AddOp(txscript.OP_EQUAL).
+		Script()
+
+	return script
 }
 
-func GetAddressFromRedeemScriptHex(redeemScriptHex string) (string, error) {
-	redeemScript, err := hex.DecodeString(fmt.Sprintf("%x", redeemScriptHex))
+func GetAddressFromRedeemScriptHex(redeemScriptHex []byte) (string, error) {
+	// Create a P2SH address
+	redeemScriptAddr, err := btcutil.NewAddressScriptHash(redeemScriptHex, &chaincfg.TestNet3Params)
 	if err != nil {
 		return "", err
 	}
-
-	redeemScriptAddr, err := btcutil.NewAddressScriptHash(redeemScript, &chaincfg.TestNet3Params)
-	if err != nil {
-		return "", err
-	}
-
 	return redeemScriptAddr.EncodeAddress(), nil
 }
 
-func ConstructTransaction(address string, privateKey *btcec.PrivateKey, amount int64) (string, error) {
-
-	keyDB := &SimpleKeyDB{privateKey: privateKey}
-
-	// Create a new transaction
-	tx := wire.NewMsgTx(wire.TxVersion)
-
-	// Add input to the transaction (unspent output from a previous transaction)
-	prevOutHash, _ := chainhash.NewHashFromStr("67868b3b2b8e2c85893bd82e263c940bb18ee1371ffe4602313f41ddd675c590")
-	prevOutPoint := wire.NewOutPoint(prevOutHash, 0)
-	txIn := wire.NewTxIn(prevOutPoint, nil, nil)
-	tx.AddTxIn(txIn)
-
-	// Add output to the transaction (sending to the derived address)
-	decodedAddress, err := btcutil.DecodeAddress(address, &chaincfg.TestNet3Params)
-	if err != nil {
-		return "", err
-	}
-	pkScript, _ := txscript.PayToAddrScript(decodedAddress)
-	txOut := wire.NewTxOut(amount, pkScript)
-	tx.AddTxOut(txOut)
-
-	redeemScript := GenerateRedeemScriptHex("Btrust Builders")
-
-	// sign the transaction
-	signTx(tx, redeemScript, keyDB)
-
-	// Serialize the transaction to hex
-	buf := new(bytes.Buffer)
-	err = tx.Serialize(buf)
-	if err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(buf.Bytes()), nil
-}
-
-func SpendTransactionOriginalTxHex(txnHex string, redeemScriptHex string, destAddress string,
-	changeAddress string, privateKey *btcec.PrivateKey) (string, error) {
-
-	keyDB := &SimpleKeyDB{privateKey: privateKey}
-
-	// Deserialize the original transaction
-	originalTxBytes, _ := hex.DecodeString(txnHex)
-	originalTx := wire.MsgTx{}
-	err := originalTx.Deserialize(bytes.NewReader(originalTxBytes))
-	if err != nil {
-		return "", err
-	}
+func ConstructTransaction(redeemScriptHex []byte, address string, privateKey string, amount int64) (string, error) {
+	//derive the private Key
+	privateKeyEn := parsePrivateKey(privateKey)
 
 	// Create a new transaction
 	tx := wire.NewMsgTx(wire.TxVersion)
+
+	// Add an output with the P2SH redeem script
+	tx.AddTxOut(&wire.TxOut{
+		Value:    amount,
+		PkScript: redeemScriptHex,
+	})
 
 	// Get the transaction hash
-	txHash := originalTx.TxHash()
+	txHash := tx.TxHash()
 
-	// Add input to the transaction (using the output of the previous transaction)
-	outPoint := wire.NewOutPoint(&txHash, 0)
-	txIn := wire.NewTxIn(outPoint, nil, nil)
-	tx.AddTxIn(txIn)
+	// Create the funding transaction
+	fundingTx := wire.NewMsgTx(wire.TxVersion)
+	fundingTx.AddTxIn(&wire.TxIn{
+		PreviousOutPoint: *wire.NewOutPoint(&txHash, 0),
+		SignatureScript:  redeemScriptHex,
+	})
+	fundingTx.AddTxOut(&wire.TxOut{
+		Value:    90000000, // 0.9 BTC change
+		PkScript: []byte(address),
+	})
 
-	// Add output to the transaction (spending to the destination address)
-	decodedDestAddress, err := btcutil.DecodeAddress(destAddress, &chaincfg.TestNet3Params)
-	if err != nil {
-		return "", err
-	}
-	destPkScript, _ := txscript.PayToAddrScript(decodedDestAddress)
-	txOutDest := wire.NewTxOut(originalTx.TxOut[0].Value-1000, destPkScript)
-	tx.AddTxOut(txOutDest)
-
-	// Add another output for change
-	decodedChangeAddress, err := btcutil.DecodeAddress(changeAddress, &chaincfg.TestNet3Params)
-	if err != nil {
-		return "", err
-	}
-	changePkScript, _ := txscript.PayToAddrScript(decodedChangeAddress)
-	txOutChange := wire.NewTxOut(1000, changePkScript)
-	tx.AddTxOut(txOutChange)
-
-	//sign the transaction
-	signTx(tx, redeemScriptHex, keyDB)
-
-	// Serialize the spending transaction to hex
-	buf := new(bytes.Buffer)
-	err = tx.Serialize(buf)
-	if err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(buf.Bytes()), nil
-
-}
-
-// SimpleKeyDB is a simple implementation of the KeyDB interface.
-type SimpleKeyDB struct {
-	privateKey *btcec.PrivateKey
-}
-
-// GetKey retrieves the private key associated with a given address.
-func (k *SimpleKeyDB) GetKey(address btcutil.Address) (*btcec.PrivateKey, bool, error) {
-	return k.privateKey, true, nil
-}
-
-func signTx(tx *wire.MsgTx, redeemScriptHex string, keyDB txscript.KeyDB) {
-	redeemScript, err := hex.DecodeString(redeemScriptHex)
-	if err != nil {
-		log.Fatal("Error decoding redeem script:", err)
-	}
-
-	hashType := txscript.SigHashAll
-	sigScript, err := txscript.SignTxOutput(&chaincfg.TestNet3Params, tx, 0, redeemScript, hashType, keyDB, nil, nil)
+	// Sign the funding transaction
+	signature, err := txscript.RawTxInSignature(fundingTx, 0, redeemScriptHex, txscript.SigHashAll, privateKeyEn)
 	if err != nil {
 		log.Fatal("Error signing transaction:", err)
 	}
+	fundingTx.TxIn[0].SignatureScript, _ = txscript.NewScriptBuilder().
+		AddData(signature).
+		Script()
 
-	tx.TxIn[0].SignatureScript = sigScript
+	// Serialize the transaction to hex
+	buf := new(bytes.Buffer)
+	err = fundingTx.Serialize(buf)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(buf.Bytes()), nil
+}
+
+func SpendTransactionOriginalTxHex(txnHex string, redeemScriptHex []byte, changeAddress, privateKey string) (string, error) {
+
+	//derive the private Key
+	privateKeyEn := parsePrivateKey(privateKey)
+
+	//derive the output
+	prevTxHash, err := chainhash.NewHashFromStr(txnHex)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Create a new wire.OutPoint instance
+	outpoint := wire.NewOutPoint(prevTxHash, 0)
+
+	// Create a spending transaction
+	spendingTx := wire.NewMsgTx(wire.TxVersion)
+
+	// Add an input referencing the output of the funding transaction
+	spendingTx.AddTxIn(&wire.TxIn{
+		PreviousOutPoint: *outpoint,
+		SignatureScript:  redeemScriptHex,
+	})
+
+	// Add an output
+	spendingTx.AddTxOut(&wire.TxOut{
+		Value:    90000000, // 0.9 BTC
+		PkScript: []byte(changeAddress),
+	})
+
+	// Sign the spending transaction
+	signature, err := txscript.RawTxInSignature(spendingTx, 0, redeemScriptHex, txscript.SigHashAll, privateKeyEn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	spendingTx.TxIn[0].SignatureScript, _ = txscript.NewScriptBuilder().
+		AddData(signature).
+		Script()
+
+	// Serialize the spending transaction to hex
+	buf := new(bytes.Buffer)
+	err = spendingTx.Serialize(buf)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(buf.Bytes()), nil
+
+}
+
+func signTx(tx *wire.MsgTx, privKey *btcec.PrivateKey, redeemScript []byte) (*wire.MsgTx, *wire.OutPoint) {
+	txid := tx.TxHash()
+	signature, err := txscript.RawTxInSignature(tx, 0, redeemScript, txscript.SigHashAll, privKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tx.TxIn[0].SignatureScript, _ = txscript.NewScriptBuilder().
+		AddData(signature).
+		Script()
+
+	return tx, wire.NewOutPoint(&txid, 0)
+}
+
+func parsePrivateKey(privateKeyHx string) *btcec.PrivateKey {
+	// Parse the private key
+	privateKey, _ := btcec.PrivKeyFromBytes([]byte(privateKeyHx))
+	return privateKey
 }
